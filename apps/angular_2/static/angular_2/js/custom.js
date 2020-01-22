02326119597cdb59bbb13dfa4cf55ef00d4c8a69
roberto <roberto.rocuantv@gmail.com>
2020-01-21 22:54:27 -0300

    message on angularjs added

--- apps/angular_2/static/angular_2/js/custom.js
+++ apps/angular_2/static/angular_2/js/custom.js
@@ -1,16 +1,23 @@
 var app = angular.module("testAngular", []);
-app.controller("testAngularController", function($scope, $http) {
+app.controller("testAngularController", function($scope, $http, $sce) {
 	var createOrNo = false
-	$scope.mainDict = {};
-
+	
 	$scope.saveSubject = function(item) {
-		var testingData = {testing_id: $scope.item.test, images_id: imageId}
+		if (angular.isUndefined($scope.item)) {
+    		$scope.Message = $sce.trustAsHtml("<div class='text-center'> You must select an option.</div>");		
+  		} 
 
-		$http.get('/api/angular_2/mixput/' + imageId).then(function successCallback(response) {
-			$http.patch('/api/angular_2/mixput/' + imageId, testingData)
-		}, function errorCallback(response) {
-			$http.post('/api/angular_2/mix/', testingData)	
-		});
+  		else {
+
+			var testingData = {testing_id: $scope.item.test, images_id: imageId}
+
+			$http.get('/api/angular_2/mixput/' + imageId).then(function successCallback(response) {
+				$http.patch('/api/angular_2/mixput/' + imageId, testingData)
+				$scope.Message = $sce.trustAsHtml("<div class='text-center'> Successfully updated.</div>");		
+			}, function errorCallback(response) {
+				$http.post('/api/angular_2/mix/', testingData)	
+				$scope.Message = $sce.trustAsHtml("<div class='text-center'> Successfully added.</div>");		
+			});
+		}
 	};
-
 });
